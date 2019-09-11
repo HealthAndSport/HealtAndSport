86036e363188978f048cb156edd92a25e25871d3
TahaAlTamimi <altamimitmo@gmail.com>
2019-09-11 14:06:33 +0300

    Youtube API clean code And Add Nav bar

--- MainApp/componenet/AppYoutube/AppYoutube.js
+++ MainApp/componenet/AppYoutube/AppYoutube.js
@@ -6,53 +6,48 @@ import SearchBar from './SearchBar';
 import Video from './Video';
 
 export class AppTamimi extends Component {
+
     state = {
+
         videos:""
-    }
+            
+            }
 
     find=(title)=>{
-        // console.log(title)
-        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=${title}
+        
+        axios.get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q=${title}
         &type=video&key=AIzaSyBdVut9QCzqAHBzfDEh30yUp4E529som6s
-    `)
-            .then(res => {
-                // console.log(res.data.items);
+    `           )
+
+            .then ( res  =>  {
+
                 this.setState({
-                  videos: res.data.items
 
-                }
-                )
+                 videos: res.data.items
 
-    }
+                            })
 
-            )}
+            }
 
-    // componentDidMount() {
-    //     axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=healthy sports&type=video&key=AIzaSyDJfV4Io9lIg28IVDUllIywSI1_v6qWwSk`)
-    //         .then(res => {
-    //             console.log(res.data.items);
-    //             this.setState({
-    //                 videos: res.data.items
+         )}
 
-    //             }
-    //             )
-    //             console.log(this.state.videos[0].id.kind)
-    //         })
-    //         .catch(err => console.log(err))
-
-    // }
-  
+    
 
     render() {
-        // console.log(this.state.test[0].name)
+
         return (
+
             <>
+
                 <NavBar/>
-                <SearchBar find={this.find}/>
-               {this.state.videos.length===0 &&<Card/>}
+
+                <SearchBar find = { this.find }/>
+
+               { this.state.videos.length === 0 && <Card/> }
               
              
-               {this.state.videos.length>0 && this.state.videos.map((elem,index) =><Video vid={elem} key={index}/>)} 
+               { this.state.videos.length > 0 && this.state.videos.map ( ( elem, index ) => <Video vid= { elem } key = { index } />)} 
                 
 
               
--- MainApp/componenet/AppYoutube/VideoShow.js
+++ MainApp/componenet/AppYoutube/VideoShow.js
@@ -3,28 +3,36 @@ import React, { Component } from 'react'
 
 
 export class VideoShow extends Component {
-    state = {
-        
-
-    };
+    
 
     render() {
-        // console.log(this.props.video)
+
         return (
+
             <>
             
 
 
-              {this.props.video.map((item,index)=>{
+              { this.props.video.map (( item,index ) => {
+
                 return (
+
                     <div key={index} className="row ">
-                        <div className="card mb-4 shadow-lg col-md-6">
-                            <div className="card-body">
-                            <h4 className="text-center mb-4">{item.snippet.title}</h4>
-                            <iframe width="560"
-                            title="shown"
-                            height="315" src= {`//www.youtube.com/embed/${item.id.videoId}`} allowFullScreen key={index}></iframe>
-                            <h6 className="text-center mb-4">{item.snippet.description}</h6>
+
+                         <div className="card mb-4 shadow-lg col-md-6">
+
+                               <div className="card-body">
+
+                                    <h4 className="text-center mb-4">{ item.snippet.title }</h4>
+                                        <iframe 
+                                        
+                                        width="560"
+                                                
+                                         title="shown"
+                                                
+                                        height="315" src= {`//www.youtube.com/embed/${item.id.videoId}`} allowFullScreen key={index}></iframe>
+                                                     
+                                     <h6 className="text-center mb-4">{item.snippet.description}</h6>
 
                             </div>
                         
