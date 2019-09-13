31e78a8c593cf957e1f7854d755704e637a3f967
TahaAlTamimi <altamimitmo@gmail.com>
2019-09-13 16:12:36 +0300

    Add food part

--- healthy/src/App.js
+++ healthy/src/App.js
@@ -3,6 +3,7 @@ import AppYoutube from './componenet/AppYoutube/AppYoutube'
 import Post from './componenet/Momani/Post'
 import AppMomani from './componenet/Momani/AppMomani'
 import './App.css';
+// import AppNajdawe from './componenet/Najdawe/AppNajdawe'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       
       <AppYoutube/>
       <Post/>
+      {/* <AppNajdawe/> */}
       {/* <AppMomani/> */}
     </div>
   );
